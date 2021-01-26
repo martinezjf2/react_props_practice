9f866439217cdf52a41dfe79e9b1d55c24ea2679
Jeffrey Martinez <57267088+martinezjf2@users.noreply.github.com>
2021-01-26 18:29:04 -0500

    passed down props to card component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,7 +6,12 @@ function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <Card />
+      <Card
+        name="Beyonce"
+        img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+        phoneNumber="+123 456 789"
+        email="b@beyonce.com"
+      />
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,16 @@
 import React from "react";
 
 function Card(props) {
+  console.log(props);
   return (
     <div className="card">
       <div className="top">
-        <h2 className="name">Beyonce</h2>
-        <img
-          src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-          alt="avatar_img"
-          className="circle-img"
-        />
+        <h2 className="name">{props.name}</h2>
+        <img src={props.img} alt="avatar_img" className="circle-img" />
       </div>
       <div className="bottom">
-        <p className="info">+123 456 789</p>
-        <p className="info">b@beyonce.com</p>
+        <p className="info">{props.phoneNumber}</p>
+        <p className="info">{props.email}</p>
       </div>
     </div>
   );
