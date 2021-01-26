b9b0f6d7b9889b76fbd45f878f01b9184b0c8363
Jeffrey Martinez <57267088+martinezjf2@users.noreply.github.com>
2021-01-26 18:36:10 -0500

    created more cards

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,6 +13,18 @@ function App() {
         phoneNumber={contacts[0].phone}
         email={contacts[0].email}
       />
+      <Card
+        name={contacts[1].name}
+        img={contacts[1].imgURL}
+        phoneNumber={contacts[1].phone}
+        email={contacts[1].email}
+      />
+      <Card
+        name={contacts[2].name}
+        img={contacts[2].imgURL}
+        phoneNumber={contacts[2].phone}
+        email={contacts[2].email}
+      />
     </div>
   );
 }
