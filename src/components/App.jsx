53ee542a4180e541076bf77d0bfe98a768426ac2
Jeffrey Martinez <57267088+martinezjf2@users.noreply.github.com>
2021-01-26 18:35:29 -0500

    imported contacts and called them in the app.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,14 +3,15 @@ import Card from "./Card";
 import contacts from "../contacts";
 
 function App() {
+  console.log(contacts);
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
       <Card
-        name="Beyonce"
-        img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-        phoneNumber="+123 456 789"
-        email="b@beyonce.com"
+        name={contacts[0].name}
+        img={contacts[0].imgURL}
+        phoneNumber={contacts[0].phone}
+        email={contacts[0].email}
       />
     </div>
   );
