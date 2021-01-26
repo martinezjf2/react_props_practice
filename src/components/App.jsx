11a1ecd19ab26e731f78895a8239f08d25153a4e
Jeffrey Martinez <57267088+martinezjf2@users.noreply.github.com>
2021-01-26 18:25:33 -0500

    created card component, import card to app.js and called it

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,12 @@
 import React from "react";
+import Card from "./Card";
 // import { contacts } from "../contacts";
 
-function App(props) {
-  console.log(props);
+function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-
-      <div className="card">
-        <div className="top">
-          <h2 className="name">Beyonce</h2>
-          <img
-            src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-            alt="avatar_img"
-            className="circle-img"
-          />
-        </div>
-        <div className="bottom">
-          <p className="info">+123 456 789</p>
-          <p className="info">b@beyonce.com</p>
-        </div>
-      </div>
+      <Card />
     </div>
   );
 }
